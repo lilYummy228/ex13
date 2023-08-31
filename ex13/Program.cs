d652e0ee8a55f0f9a71bb450b1c783c128b9c791
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-31 13:49:31 +0500

    Доработка

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -9,7 +9,27 @@ namespace ex13
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(new string(symbol, name.Length + 2));
+            Console.Write($"{symbol}{name}{symbol}\n");
+            Console.WriteLine(new string(symbol, name.Length + 2));
+
+            for (int i = 0; i < (name.Length + 2); i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.Write($"\n{symbol}{name}{symbol}\n");
+
+            for (int i = 0; i < (name.Length + 2); i++)
+            {
+                Console.Write(symbol);
+            }
         }
     }
 }
