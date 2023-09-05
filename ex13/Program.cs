bcf5d48fde45ad050bdb75835cb36c84de552ac6
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-05 08:18:34 +0500

    123

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -10,15 +10,16 @@ namespace ex13
     {
         static void Main(string[] args)
         {
+            int border = 2;
             Console.Write("Введите имя: ");
             string name = Console.ReadLine();
             Console.Write("Введите символ: ");
             char symbol = Console.ReadKey(true).KeyChar;
             Console.WriteLine(symbol);
 
-            Console.WriteLine(new string(symbol, name.Length));
+            Console.WriteLine(new string(symbol, name.Length + border));
             Console.Write($"{symbol}{name}{symbol}\n");
-            Console.WriteLine(new string(symbol, name.Length + 2));
+            Console.WriteLine(new string(symbol, name.Length + border));
         }
     }
 }
